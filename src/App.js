4fd3779808f0ab022cda30d6403f5a33a35bc73a
Mayank Dwivedi <6703md@gmail.com>
2024-12-30 23:23:58 +0530

    Added SCSS support, updated dependencies, and included assets (fonts and images)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import './App.scss';
 
 function App() {
-  return (
+  return ( 
     <>
     <Routes>
-      <Route path="/"></Route>
+      <Route path="/" element= {<Layout />} />
 
 
     </Routes>
