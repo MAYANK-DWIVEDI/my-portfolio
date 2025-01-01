e5b2e3f5526a8ee9cd6850662c503e79b06d66ab
Mayank Dwivedi <6703md@gmail.com>
2025-01-01 23:12:44 +0530

    Added Animated letters .js and scssfile modified a few components components

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,33 @@
-import React from 'react'; // Import React
+import React, { useState } from 'react'; // Import React
 import { Link } from 'react-router-dom'; // Import Link for routing
 import LogoTitle from '../../assets/images/logo-s.png'; // Correctly imported logo
 import'./index.scss';
-const Home  = () => {
+import AnimatedLetters from '../AnimatedLetters';
 
+
+const Home  = () => {
+    const[letterClass] = useState('text-animate')
+    const nameArray = ['m','a','y','a','n','k']
+    const jobArray =['w','e','b',' ', 'd','e','v','e','l','o','p','e','r']
 
     return (
         <div className=" container home-page">
             <div className=" text-zone">
-                <h1> Hi,  <br/>  I'm
+                <h1> 
+                    <span className='letterClass'>H</span>
+                    <span className={`${letterClass} _12`}>H</span>  <br/> 
+                    
+                    <span className={`${letterClass} _13`}>I</span> 
+                    
+                    <span className={`${letterClass} _14`}>'m</span>  
                 <img src ={LogoTitle} alt="developer" /> 
-                Mayank
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15}/>
                 <br/>
-                web developer
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={22}/>
                 </h1>
                 <h2> Frontend developer /javascript </h2>
                 <Link to="/contact" className='flat-button'>  Cotact Me</Link>
